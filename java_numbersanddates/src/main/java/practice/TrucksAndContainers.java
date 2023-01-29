eb1f43d4161bd6ecdc69b808bc30f13883ed6f18
Boris Kuptsevich <esrb2000@bk.ru>
2023-01-29 22:20:12 +0700

    v 1.1

--- java_numbersanddates/src/main/java/practice/TrucksAndContainers.java
+++ java_numbersanddates/src/main/java/practice/TrucksAndContainers.java
@@ -9,7 +9,20 @@ public class TrucksAndContainers {
 
         //получение количество коробок от пользователя
         int boxes = scanner.nextInt();
+// мой код
+        private int requiredContainer = 0;
+        private int requiredTrack = 0;
 
+        public int getRequiredContainer() {
+            requiredContainer = count/27;
+            return requiredContainer;
+        }
+
+        public int getRequiredTruck() {
+            requiredTrack = requiredContainer/12;
+            return requiredTrack;
+        }
+// мой код
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
